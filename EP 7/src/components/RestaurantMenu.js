ea408add9d367dc4ddd64c224e8975fb7fbb96ec
Darji Het <135495667+DarjiHet@users.noreply.github.com>
2025-02-17 16:17:59 +0530

    Update RestaurantMenu.js

--- EP 7/src/components/RestaurantMenu.js
+++ EP 7/src/components/RestaurantMenu.js
@@ -1,40 +1,53 @@
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
 
 const RestaurantMenu = () => {
 
-    const [restInfo, setResInfo] = useState([]);
+    const [restInfo, setResInfo] = useState(null);
 
+    const {resId} = useParams()
+    
+    //fetching data from api
     useEffect(() => {
         fetchMenu();
     },[]);
 
     const fetchMenu = async () => {
         const data = await fetch( 
-            "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.022505&lng=72.5713621&restaurantId=47589"
+            `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.022505&lng=72.5713621&restaurantId=${resId}`
         );
         const json = await data.json();
 
         console.log(json);
-        setResInfo(json);
+        //setting local state variable
+        setResInfo(json.data);
     };    
 
-    // const {name } = restInfo?.data?.cards[0]?.card?.card?.text;
-    const name = restInfo?.data?.cards[0]?.card?.card?.text;
-
-    const {avgRating, costForTwoMessage, cuisines} = restInfo?.data?.cards[2]?.card?.card?.info;
-
+    //condition for shimmer ui
     if (restInfo == null) return <Shimmer />;
+
+    //data of restaurant
+    const restaurantInfo = restInfo?.cards[2]?.card?.card?.info; 
+    const { name = "Unknown", costForTwoMessage = "N/A", cuisines = [], avgRating = "" } = restaurantInfo;
+
+    //item or menu of restaurant
+    const itemCards = restInfo?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[1]?.card?.card?.itemCards || restInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards || [];
+
+    console.log();
 
     return(
         <div className="menu">
             <h1> {name} </h1>
-            <h2> {avgRating} </h2>
-            <p>{cuisines} - {"Rs"} {costForTwoMessage} </p>
+            <h3> {avgRating} </h3>
+            <p>{cuisines.join(", ")} - {"Rs"} {costForTwoMessage} </p> 
+            <h3>Menu</h3>
             <ul>
-                <li>Biryani</li>
-                <li>Burgers</li>
-                <li>Biryani</li>
+                {
+                    itemCards.map((item) => (
+                        <li key={item.card.info.id} > {item.card.info.name} - {item.card.info.defaultPrice / 100 || item.card.info.finalPrice / 100 || item.card.info.price / 100 } </li>
+                    ))
+                }
             </ul>
         </div>
     )
